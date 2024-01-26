b4c6ce95c7b344d73ac594e0cc71036e5e553990
Adrián Darío Hidalgo Flores <adrian@bundly.dev>
2024-01-25 19:47:17 -0600

    Update import paths for courses and users actors

--- 02-react-with-backend/frontend/src/canisters/courses.ts
+++ 02-react-with-backend/frontend/src/canisters/courses.ts
@@ -1,9 +1,9 @@
 import { Canister } from "@bundly/ic-core-js";
 import { ActorSubclass } from "@dfinity/agent";
 
+import { _SERVICE } from "../declarations/courses/courses.did";
 // @ts-ignore
 import { idlFactory } from "../declarations/courses/courses.did.js";
-import { _SERVICE } from "../declarations/courses/courses.did.js";
 
 export type CoursesActor = ActorSubclass<_SERVICE>;
 
